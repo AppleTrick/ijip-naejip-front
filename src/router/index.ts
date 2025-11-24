c2c572cd84f2a3824e95878f64340ae279ad6414
arrans9591 <arrans9591@gmail.com>
2025-11-24 16:39:21 +0900

    ♻️ refactor: 뷰 디렉토리 구조 개선 - 각 뷰별 독립 폴더 구조로 재구성
    - market, home, auth, settings 모든 뷰를 각각 독립된 하위 폴더로 분리
    - 각 뷰의 컴포넌트를 해당 뷰 폴더 내 components 디렉토리로 이동
    - 총 14개 뷰, 18개 컴포넌트 재구성
    - 라우터 import 경로 전체 업데이트
    - settings/settings → settings/settings-home으로 명확성 개선
    
    개선 효과:
    - 컴포넌트 소유권 명확화
    - 모듈화 및 유지보수성 향상
    - 일관된 프로젝트 구조 확립

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,72 +4,72 @@ const routes: Array<RouteRecordRaw> = [
     {
       path: '/',
       name: 'landing',
-      component: () => import('../views/home/LandingView.vue')
+      component: () => import('../views/home/landing/LandingView.vue')
     },
     {
       path: '/home',
       name: 'home',
-      component: () => import('../views/home/HomeView.vue')
+      component: () => import('../views/home/home/HomeView.vue')
     },
     {
       path: '/map',
       name: 'market',
-      component: () => import('../views/market/MarketView.vue')
+      component: () => import('../views/market/market-map/MarketView.vue')
     },
     {
       path: '/price/:id',
       name: 'price-detail',
-      component: () => import('../views/market/PriceDetailView.vue')
+      component: () => import('../views/market/price-detail/PriceDetailView.vue')
     },
     {
       path: '/comparison',
       name: 'comparison',
-      component: () => import('../views/market/ComparisonView.vue')
+      component: () => import('../views/market/comparison/ComparisonView.vue')
     },
     {
       path: '/analysis',
       name: 'analysis',
-      component: () => import('../views/market/AnalysisView.vue')
+      component: () => import('../views/market/analysis/AnalysisView.vue')
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/auth/LoginView.vue')
+      component: () => import('../views/auth/login/LoginView.vue')
     },
     {
       path: '/signup',
       name: 'signup',
-      component: () => import('../views/auth/SignUpView.vue')
+      component: () => import('../views/auth/signup/SignUpView.vue')
     },
     {
       path: '/find-password',
       name: 'find-password',
-      component: () => import('../views/auth/FindPasswordView.vue')
+      component: () => import('../views/auth/find-password/FindPasswordView.vue')
     },
     {
       path: '/settings',
       name: 'settings',
-      component: () => import('../views/settings/SettingsView.vue')
+      component: () => import('../views/settings/settings-home/SettingsView.vue')
     },
     {
       path: '/settings/profile',
       name: 'profile-edit',
-      component: () => import('../views/settings/ProfileEditView.vue')
+      component: () => import('../views/settings/profile-edit/ProfileEditView.vue')
     },
     {
       path: '/settings/house',
       name: 'house-management',
-      component: () => import('../views/settings/HouseManagementView.vue')
+      component: () => import('../views/settings/house-management/HouseManagementView.vue')
     },
     {
       path: '/settings/notifications',
       name: 'notification-settings',
-      component: () => import('../views/settings/NotificationSettingsView.vue')
+      component: () => import('../views/settings/notification-settings/NotificationSettingsView.vue')
     },
     {
       path: '/settings/password',
       name: 'password-change',
-      component: () => import('../views/settings/PasswordChangeView.vue')
+      component: () => import('../views/settings/password-change/PasswordChangeView.vue')
     },
     {
       path: '/:pathMatch(.*)*',
